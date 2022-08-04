911607c4fa4509d03c4919cbc81a6c423bbea633
Kseniia <kseniiawest777@mail.ru>
2022-08-04 14:45:14 +0200

    feat: add js script and mailer folder for sending email

--- projects/pulse/src/js/script.js
+++ projects/pulse/src/js/script.js
@@ -84,7 +84,21 @@ $(document).ready(function(){
     
     $('input[name=phone]').mask("+7(999) 999-99-99");
 
-    
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 
   
 
