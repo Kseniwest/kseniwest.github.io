294ad0572bfb115d18f61373a4099f09a41090ed
Kseniia <kseniiawest777@mail.ru>
2022-08-04 16:22:33 +0200

    feat: add page animation for heart icon

--- projects/pulse/src/js/script.js
+++ projects/pulse/src/js/script.js
@@ -100,8 +100,15 @@ $(document).ready(function(){
         return false;
     });
 
-  
+    // Smooth scroll and pageup
 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
 
 
 
