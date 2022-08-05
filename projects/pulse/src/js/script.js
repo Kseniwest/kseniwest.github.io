ccdd5c968a68d1fe2e2e391a7b4d6e4740cde286
Kseniia <kseniiawest777@mail.ru>
2022-08-05 13:40:00 +0200

    fix: change html for style for catalog items

--- projects/pulse/src/js/script.js
+++ projects/pulse/src/js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
             $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-list').eq(i).toggleClass('catalog-list_active');
             })
         });
     };
@@ -110,11 +110,7 @@ $(document).ready(function(){
         }
     });
 
-
     //Animation
     
     new WOW().init();
-
-
-
 });
