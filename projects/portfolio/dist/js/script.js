927f723179dd7ee9d74c3eb46a0db6f9970e78db
Kseniia <kseniiawest777@mail.ru>
2022-08-12 16:56:57 +0200

    feat: add hamburger

---
+++ projects/portfolio/dist/js/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
