4ce37a637ca18ebdb69ac697bc34875afb55754d
Kseniia <kseniiawest777@mail.ru>
2022-08-17 11:00:04 +0200

    feat: add skript for ratings

--- projects/portfolio/dist/js/script.js
+++ projects/portfolio/dist/js/script.js
@@ -1,3 +1,5 @@
+// hamburger
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
@@ -8,4 +10,14 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+// skills rating
+
+const counters = document.querySelectorAll('.skills__ratings-counter');
+const lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+
 });
